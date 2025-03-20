ff2ec14f5e11856f8b6b22954d1fddf95d0c594c
jmy-k <myjuliakan@gmail.com>
2025-03-20 18:54:58 -0400

    zoom background image

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 let hitoIntro = document.querySelector("#hitoIntro");
+let bigImage = document.querySelector("#backgroundImage")
 let doorwayLink = document.querySelector("#doorwayLink");
 
-doorwayLink.addEventListener("hover", function () {
+doorwayLink.addEventListener("mouseenter", function () {
     hitoIntro.style.opacity = "1";
+    hitoIntro.style.fontSize = "3em";
     console.log("hovering");
+    bigImage.style.transform = "scale(2)";
 })
+
+doorwayLink.addEventListener("mouseleave",() =>{
+    hitoIntro.style.opacity = "0";
+    hitoIntro.style.fontSize = "1em";
+    bigImage.style.transform="scale(1)";
+})
+
